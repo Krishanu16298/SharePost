b8d5d26d8a07c266dcb80cb32edf00fcf13d4751
Krishanu Mallik Thakur <mkrishanu98@gmail.com>
2018-05-29 11:54:22 +0530

    PostMVC

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,4 @@
 <?php require_once APPROOT.'/views/include/header.php';?>
 <h1><?php echo $data['title'];?></h1>
+<p>This is a MVC for blog like pages in php.</p>
 <?php require_once APPROOT.'/views/include/footer.php';?>
